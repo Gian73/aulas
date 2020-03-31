fdb5a663766629335029c9a5e85a28ab085ee6c6
Gian73 <37905041+Gian73@users.noreply.github.com>
2020-03-30 21:28:38 -0300

    adicionando linhas para simular Debug

--- src/aula2/Main.java
+++ src/aula2/Main.java
@@ -2,31 +2,39 @@ import model.Funcionario;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args){
-		dadosFuncionarios();	
+	public static void main(String[] args) {
+		dadosFuncionarios();
 	}
 
-	public static void dadosFuncionarios(){
-	
+	public static void dadosFuncionarios() {
+
 		Scanner sc = new Scanner(System.in);
 		Funcionario funcionario;
-		int opcao =0;
-		
-		System.out.print("Escolha uma opcao : {1 criar} {2 ver} ");
-			opcao = Integer.parseInt(sc.next());
-		do {
-			if(opcao == 1){
-				funcionario = new Funcionario();
-				System.out.print("Digite um nome:");
-				funcionario.nome=sc.next();
+		int opcao = 0;
 
-			}else if(opcao ==2){
+		System.out.println("Escolha uma opcao : {1 criar} {2 ver} ");
+
+		opcao = Integer.parseInt( sc.nextLine() );
+
+		System.out.println("chegou apos a gravacao da opcao");
+
+		do {
+		System.out.println("Entrou no  DO");
+			if ( opcao == 1 ) {
+				System.out.println("Entrou no if");
+				funcionario = new Funcionario();
+				System.out.println("Digite um nome:");
+				funcionario.nome=sc.nextLine();
+				System.out.println("O funcionario digitado foi: " + funcionario.nome);
+				System.out.println("A opcao nesse momento é: " + opcao);
+				opcao = 0;
+				System.out.println("A opcao nesse momento é: " + opcao);
+
+			} else if ( opcao == 2 ) {
 				funcionario = new Funcionario();
 				System.out.println(funcionario.nome);
 			}
-	
-		}while(opcao!=0);
-	//sc.close;
+		} while ( opcao != 0 );
 	}
 }
 
