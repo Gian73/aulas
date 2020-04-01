1985005d0fd7233f048fa05d747946dc6ef13012
Gian73 <37905041+Gian73@users.noreply.github.com>
2020-03-31 21:49:14 -0300

    ainda tem erro

--- src/aula2/Main.java
+++ src/aula2/Main.java
@@ -2,35 +2,51 @@ import model.Funcionario;
 import java.util.Scanner;
 
 public class Main {
+
+
+	public static int opcao = 0;
+	public static Scanner lineScanner = new Scanner(System.in);
+
+
 	public static void main(String[] args) {
 		dadosFuncionarios();
 	}
 
+	public static void recebeOpcao(){
+
+			System.out.println("\nEscolha uma opcao : {1 criar} {2 ver} {0 sair}");
+
+			try {
+				opcao = Integer.parseInt( lineScanner.nextLine() );
+			} catch ( Exception e ) {
+				System.out.println("Opcao invalida");
+			}
+
+	}
+
 	public static void dadosFuncionarios() {
 
-		Scanner sc = new Scanner(System.in);
 		Funcionario funcionario = new Funcionario();
-		int opcao = 0;
+
+
 
 		do {
 
-		System.out.println();
-		System.out.println("Escolha uma opcao : {1 criar} {2 ver} {0 sair}");
-		opcao = Integer.parseInt( sc.nextLine() );
+			recebeOpcao();
 
 			if ( opcao == 1 ) {
 
-				funcionario = new Funcionario();
-				System.out.println();
-				System.out.println("Digite um nome:");
-				funcionario.nome=sc.nextLine();
+				System.out.println("\nDigite um nome:");
+				funcionario.nome=lineScanner.nextLine();
 
 			} else if ( opcao == 2 ) {
+
 				if(funcionario.nome!=null){
-					System.out.println();
-					System.out.println("Abaixo segue o nome digitado: ");
-					System.out.println(funcionario.nome);
+
+					System.out.println("\nAbaixo segue o nome digitado:\n" + funcionario.nome);
+
 				}
+
 			}
 
 		} while ( opcao != 0 );
