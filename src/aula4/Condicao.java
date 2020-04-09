ce604e025ccccb2905dc16b13b3a484f9176400c
Gian73 <37905041+Gian73@users.noreply.github.com>
2020-04-08 22:16:57 -0300

    utilizando metodo e if else com for

--- src/aula4/Condicao.java
+++ src/aula4/Condicao.java
@@ -6,21 +6,15 @@ public class Condicao {
 	public static final long IDADE_MAXIMA = 50;
 	
 	public static void main(String[] args) {
-		validaEntrada(0);
-		validaEntrada(15);
-		validaEntrada(18);
-		validaEntrada(21);
-		validaEntrada(40);
-		validaEntrada(50);
-		validaEntrada(51);
-		validaEntrada(60);
-		validaEntrada(100);
+		for( int i=0; i<=100; i++ ) {
+			validaEntrada(i);
+		}
 	}
 	
 	public static void validaEntrada( int idade ) {
-		if  ( idade <IDADE_MINIMA ||  idade > IDADE_MAXIMA )  {
+		if( idade < IDADE_MINIMA || idade > IDADE_MAXIMA ) {
 			System.out.println("Com a idade " + idade + " Você foi banido");
-		} else {
+		}else{
 			System.out.println("Com a idade " + idade + " Você foi Liberado");
 		}
 	}
